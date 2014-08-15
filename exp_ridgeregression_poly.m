dfe855ca368ad5e80fdbb1661a7d30071624f8c6
Ryota Tomioka <ryotat@Ryota-no-MacBook-Air.local>
2014-08-15 22:26:36 +0200

    Removed True estimator covariance. Explicitly learns the bias term. demo mode. Trying to make it more general than ridge regression by allowing different loss function.

--- exp_ridgeregression_poly.m
+++ exp_ridgeregression_poly.m
@@ -1,80 +1,96 @@
 % number of samples
-n=20;
+n=200;
+ntest=1000;
 
 N=100;
 
+% loss function
+lossfun=@(y,f)(y-f).^2;
+
+% True function
+fstar=@(x)x.^3-x.^2-x;
+
+% order of polynomial used in learning
+polyorder=10;
+
+% Input distribution
+samplex=@(n)bsxfun(@power, randn(n,1), 1:polyorder);
+
 % Indices of coefficients to be visualized
-ix=[2,3];
+ix=[1,2];
 
 % noise standard deviation
 sigma=1;
 
 % Regularization parameter
-lmd = 1e-6;
-
-% order of polynomial used in learning
-polyorder=3;
-
-% True function
-fstar=@(x)x.^3-x.^2-x;
-
-% Test points
-xx=bsxfun(@power, (-2:0.1:2)', 0:polyorder);
+lmd = 1;
 
 % input variables
-X=bsxfun(@power, randn(n, 1), 0:polyorder);
-ytrue=fstar(X(:,2));
+X=samplex(n);
+ytrue=fstar(X(:,1));
 
-% True estimator covariance (assuming gaussian inputs)
-mus=arrayfun(@(k)(mod(k,2)==0)*prod(1:2:k-1),1:polyorder*2);
-H=hankel(0:polyorder, polyorder:polyorder*2); H(1)=2;
-XX=arrayfun(@(x)mus(x), H);
-Sigmastar = inv(XX+lmd*eye(polyorder+1))*XX*inv(XX+lmd*eye(polyorder+1));
-Sigmastar=(Sigmastar+Sigmastar')/2;
+% Test points
+xx=bsxfun(@power, (-2:0.1:2)', 1:polyorder);
+Xtest=samplex(ntest);
+ytesttrue=fstar(Xtest(:,1));
 
+% Demo mode
+demo=1;
 
 W=zeros(polyorder+1, N); % +1 is the bias term
-Coeff=cell(1,N);
+err=zeros(1,N);
 figure;
 kk=1;
-xl=[min(xx(:,2)), max(xx(:,2))]; yl=[min(fstar(xx(:,2)))-1, max(fstar(xx(:,2)))+1];
-while 1
+xl=[min(xx(:,1)), max(xx(:,1))]; yl=[min(fstar(xx(:,1)))-1, max(fstar(xx(:,1)))+1];
+while demo || kk<=N
   km=mod1(kk,N);
   % sample output variables
   Y=ytrue+sigma*randn(n, 1);
 
   % Train ridge regression
-  C=train_ridge(X, Y, lmd, struct('center', 0));
-  W(:,km)=C.w;
+  C=train_ridge(X, Y, lmd, struct('center', 1));
+  W(:,km)=[C.w; C.bias];
 
+  % Compute test error
+  err(km)=mean(lossfun(ytesttrue,Xtest*C.w+C.bias));
+  
   % Plot the samples, learned function, the true function
   subplot(1,2,1);
   if length(get(gca,'children'))>0
     xl=xlim; yl=ylim;
   end
-  plot(X(:,2), Y, 'x', ...
-       xx(:,2), xx*C.w, '-.', ...
-       xx(:,2), fstar(xx(:,2)), 'm--', 'linewidth', 2);
+  plot(X(:,1), Y, 'x', ...
+       xx(:,1), xx*C.w+C.bias, '-.', ...
+       xx(:,1), fstar(xx(:,1)), 'm--', 'linewidth', 2);
   xlim(xl); ylim(yl);
   grid on;
   set(gca,'fontsize',14);
   xlabel('Input');
   ylabel('Output');
   title(sprintf('n=%d d=%d', n, polyorder+1));
-
+  
   % Plot the estimated coefficients
-  subplot(1,2,2); cla;
+  subplot(1,2,2);
   P=W(ix,:);
   plot(P(1,1:min(kk,N)), P(2,1:min(kk,N)), 'x', 'color', ...
        [.5 .5 .5], 'linewidth', 2);
   hold on;
   plot(P(1,km), P(2,km), 'x', 'linewidth', 2);
+  hold off;
   axis equal; grid on;
   set(gca,'fontsize',14);
   xlabel('Coefficient for x');
   ylabel('Coefficient for x^2');
-
-  pause(0.5);
+  title(sprintf('test err=%g', err(km)));
+  %  title(sprintf('test err=%g %s %g', ...
+%                                     mean(err(1:min(kk,N))), char(177), std(err(1:min(kk,N)))), 'fontsize', 16);
+  
+  if err(km)>10
+    break;
+  end
+  if demo
+    pause(0.5);
+  end
   kk=kk+1;
 end
   
