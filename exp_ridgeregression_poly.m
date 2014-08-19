f1090809fdfad37016caf42159b2627657f55bf8
Ryota Tomioka <ryotat@users.noreply.github.com>
2014-08-19 19:01:33 +0200

    Bias is treated as one of the covariates.

--- exp_ridgeregression_poly.m
+++ exp_ridgeregression_poly.m
@@ -18,22 +18,22 @@ fout=@(z)z+sigma*randn(n, 1);
 lossfun=@(y,f)(y-f).^2;
 
 % Input distribution
-samplex=@(n)bsxfun(@power, randn(n,1), 1:polyorder);
+samplex=@(n)bsxfun(@power, randn(n,1), polyorder:-1:0);
 
 % Indices of coefficients to be visualized
-ix=[1,2];
+ix=[polyorder,polyorder-1];
 
 % Regularization parameter
 lmd = 1e-5;
 
 % input variables
 X=samplex(n);
-ytrue=fstar(X(:,1));
+ytrue=fstar(X(:,end-1));
 
 % Test points
-xx=bsxfun(@power, (-5:.1:5)', 1:polyorder);
+xx=bsxfun(@power, (-5:.1:5)', polyorder:-1:0);
 Xtest=samplex(ntest);
-ytesttrue=fstar(Xtest(:,1));
+ytesttrue=fstar(Xtest(:,end-1));
 
 % Demo mode
 demo=1;
@@ -42,15 +42,15 @@ W=zeros(polyorder+1, N); % +1 is the bias term
 err=zeros(1,N);
 figure;
 kk=1;
-xl=[min(xx(:,1)), max(xx(:,1))]; yl=[-2, 2];
+xl=[min(xx(:,end-1)), max(xx(:,end-1))]; yl=[-2, 2];
 while demo || kk<=N
   km=mod1(kk,N);
   % sample output variables
   Y=fout(ytrue);
 
   % Train ridge regression
-  C=train_ridge(X, Y, lmd, struct('center', 1));
-  W(:,km)=[C.w; C.bias];
+  C=train_ridge(X, Y, lmd, struct('center', 0));
+  W(:,km)=C.w;
 
   % Compute test error
   err(km)=mean(lossfun(ytesttrue,Xtest*C.w+C.bias));
@@ -60,9 +60,9 @@ while demo || kk<=N
   if length(get(gca,'children'))>0
     xl=xlim; yl=ylim;
   end
-  h=plot(xx(:,1), fstar(xx(:,1)), '--', ...
-       xx(:,1), xx*C.w+C.bias, '-.', ...
-       X(:,1), Y, 'bx', 'linewidth', 2);
+  h=plot(xx(:,end-1), fstar(xx(:,end-1)), '--', ...
+       xx(:,end-1), xx*C.w, '-.', ...
+       X(:,end-1), Y, 'bx', 'linewidth', 2);
   set(h(1),'color',[.5 .5 .5]);
   xlim(xl); ylim(yl);
   grid on;
@@ -79,12 +79,13 @@ while demo || kk<=N
   plot(P(1,1:min(kk,N)), P(2,1:min(kk,N)), 'x', 'color', ...
        [.5 .5 .5], 'linewidth', 2);
   hold on;
+  plotEllipse(mean(W(ix,1:min(kk,N)),2), cov(W(ix,1:min(kk,N))'), [0 .5 .5], 1, 6);
   plot(P(1,km), P(2,km), 'x', 'linewidth', 2);
   hold off;
   axis equal; grid on;
   set(gca,'fontsize',14);
-  xlabel('Coefficient for x');
-  ylabel('Coefficient for x^2');
+  xlabel(sprintf('Coefficient for x^%d',polyorder+1-ix(1)));
+  ylabel(sprintf('Coefficient for x^%d',polyorder+1-ix(2)));
   title(sprintf('test err=%g (mean: %g)', err(km), mean(err(1:min(kk,N)))));
   %  title(sprintf('test err=%g %s %g', ...
 %                                     mean(err(1:min(kk,N))), char(177), std(err(1:min(kk,N)))), 'fontsize', 16);
