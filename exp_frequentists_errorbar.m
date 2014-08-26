4a3e6585c24d77d50d6364e34f289f1ca54238d6
Ryota Tomioka <ryotat@users.noreply.github.com>
2014-08-26 20:36:53 +0200

    Fixed a bug lmd should have been lmdn.

--- exp_frequentists_errorbar.m
+++ exp_frequentists_errorbar.m
@@ -21,9 +21,6 @@ lossfun=@(y,f)(y-f).^2;
 % Input distribution
 samplex=@(n)bsxfun(@power, randn(n,1), polyorder:-1:0);
 
-% Indices of coefficients to be visualized
-ix=[1, 2];
-
 % Regularization parameter
 lmd = 1e-5;
 
@@ -59,8 +56,8 @@ while demo || kk<=N
 
   % Compute predicted test error
   d=polyorder+1; lmdn=lmd/n; Sigma=X'*X/n; S=inv(Sigma+lmdn*eye(d));
-  pv=lmd^2*(norm(w0)^2)*sum((xx*S).^2,2)+sigma^2/n*diag(xx*S*Sigma*S*xx');
-
+  pv=lmdn^2*(norm(w0)^2)*sum((xx*S).^2,2)+sigma^2/n*diag(xx*S*Sigma*S*xx');
+  
   % Compute test error
   err(km)=mean(lossfun(ytesttrue,Xtest*C.w+C.bias));
   
